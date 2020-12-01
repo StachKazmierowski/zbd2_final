d9552ff27350c40f31bfc8141b86e742c4feff23
Stach <staho.kazmierowski@gmail.com>
2020-12-01 20:21:35 +0000

    cutting head of file, Time: and ms

--- results/cut_head.sh
+++ results/cut_head.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 for file in *.sql
 do
-	tail -n +9 $file > cut$file
+	tail -n +9 $file  > cut$file
 	rm $file
+	sed -i 's/Time: //g' cut$file
+	sed -i 's/ms//g' cut$file
 	cp cut$file $file
 	rm cut$file
 
